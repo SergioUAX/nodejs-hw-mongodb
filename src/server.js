5bb0e7f9ed163f98af7d766aaa4f8b0c3b38364c
SergioUAX <itserver.ua@gmail.com>
2025-06-12 18:02:28 +0200

    part1

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { getEnvVar } from './utils/getEnvVar.js';
-import { getAllContacts, getContactById } from './services/contacts.js';
+import contactsRouter from './routers/contacts.js';
 
 
 const PORT = Number(getEnvVar('PORT', '3000'));
@@ -25,32 +25,7 @@ export const setupServer = () => {
     console.log(`Server is running on port ${PORT}`);
   });
 
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContacts();
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
-  });
-
-  app.get('/contacts/:contactId', async (req, res, next) => {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
-
-   	if (!contact) {
-	    res.status(404).json({
-		  message: 'Contact not found'
-	  });
-	  return;
-	}
-
-    res.status(200).json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId} !`,
-      data: contact,
-    });
-  });
+  app.use(contactsRouter);
 
   app.use((req, res) => {
     res.status(404).json({ message: 'Not found' });
