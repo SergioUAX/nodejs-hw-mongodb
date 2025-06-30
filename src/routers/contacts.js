3f4c603dc33c804b4c38694b475c3f87631e8454
SergioUAX <itserver.ua@gmail.com>
2025-06-30 22:14:04 +0200

    part2

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -8,21 +8,34 @@ import {
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { validateBody } from '../middlewares/validateBody.js';
-import { createContactSchema } from '../validation/contacts.js';
+import { isValidId } from '../middlewares/isValidId.js';
+import {
+    createContactSchema,
+    updateContactSchema
+} from '../validation/contacts.js';
 
 const router = Router();
 
 router.get('/contacts', ctrlWrapper(getContactsController));
-router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+router.get(
+    '/contacts/:contactId',
+    isValidId,
+    ctrlWrapper(getContactByIdController)
+);
 router.post(
     '/contacts',
     validateBody(createContactSchema),
     ctrlWrapper(createContactController)
 );
-router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
+router.delete(
+    '/contacts/:contactId',
+    isValidId,
+    ctrlWrapper(deleteContactController)
+);
 router.patch(
     '/contacts/:contactId',
-    validateBody(createContactSchema),
+    isValidId,
+    validateBody(updateContactSchema),
     ctrlWrapper(patchContactController)
 );
 
