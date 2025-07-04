fb79e978afd7c827407faaa68eea2f8eb1429ada
SergioUAX <itserver.ua@gmail.com>
2025-07-04 11:18:14 +0200

    part7_final

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,7 +1,6 @@
 import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
-  userId: Joi.string().required(), //new
   name: Joi.string().min(3).max(20).required(),
   phoneNumber: Joi.string().min(3).max(20).required(),
   email: Joi.string().min(3).max(20).required(),
@@ -10,7 +9,6 @@ export const createContactSchema = Joi.object({
 });
 
 export const updateContactSchema = Joi.object({
-    userId: Joi.string(), //new
     name: Joi.string().min(3).max(20),
     phoneNumber: Joi.string().min(3).max(20),
     email: Joi.string().min(3).max(20),
